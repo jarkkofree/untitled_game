16ab8684e5ea7c1a9d05376105592c861728edcf
jarkkofree <125302859+jarkkofree@users.noreply.github.com>
2024-08-24 20:17:25 -0700

    comment for future feature

--- Assets/Scripts/ContextMenuController.cs
+++ Assets/Scripts/ContextMenuController.cs
@@ -10,6 +10,21 @@ public class ContextMenuController : MonoBehaviour, IPointerClickHandler
     {
         if (eventData.button == PointerEventData.InputButton.Right)
         {
+            GameObject clickedObject = eventData.pointerPress;
+
+            // if another object has been left-click selected (selection)
+               // if selection is ship owned by player
+                  // if right clicked object is a station
+                     // then show additional context menu section to select command
+                     // appropriate for ship-station interactions (dock, trade)
+                  // else if right clicked object is ship
+                     // then show additional context menu section to select command
+                     // appropriate for ship-ship interactions (follow, attack, defend)
+                  // else if right clicked object is field
+                     // then show additional context menu section to select command
+                     // appropriate for ship-field interactions (mine, prospect)
+                  // ...
+
             if (_contextMenu == null)
             {
                 _contextMenu = Instantiate(ContextMenuPrefab, transform);
